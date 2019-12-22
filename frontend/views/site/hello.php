7adefccfd7151d20e2cdf653b88f257e880c7e64
Pavel <leniviy.putnik@gmail.com>
2019-12-22 10:56:35 +0400

    first commit

---
+++ frontend/views/site/hello.php
@@ -0,0 +1,14 @@
+<?php
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Hello, world!';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>This is the "Hello, world!" page. It is copy-pasted for 1 homework.</p>
+
+</div>
