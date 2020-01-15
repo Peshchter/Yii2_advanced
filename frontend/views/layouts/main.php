9a0bdaa52f692e5d53de0e96043c7542f245c6cb
Pavel <leniviy.putnik@gmail.com>
2020-01-15 22:24:20 +0400

    Добавлен чат с базой

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -37,6 +37,7 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
+        ['label' => 'Chat', 'url' => ['/chat/index']],
         ['label' => 'About', 'url' => ['/site/about']],
         ['label' => 'Hello, world', 'url' => ['/site/hello']],
         ['label' => 'Contact', 'url' => ['/site/contact']],
